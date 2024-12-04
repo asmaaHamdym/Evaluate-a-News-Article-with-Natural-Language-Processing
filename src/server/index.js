04f8111a6088aeca1310d29f83928cae2a1d430a
asmaaHamdym <asmahamdym@gmail.com>
2024-12-04 17:20:36 +0200

    Initial Commit

---
+++ src/server/index.js
@@ -0,0 +1,27 @@
+var path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.static("dist"));
+
+console.log(__dirname);
+
+// Variables for url and api key
+
+app.get("/", function (req, res) {
+  res.sendFile("dist/index.html");
+});
+
+// POST Route
+
+// Designates what port the app will listen to for incoming requests
+app.listen(8000, function () {
+  console.log("Example app listening on port 8000!");
+});
