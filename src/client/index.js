799e2ac4445538e8e7e57fab9bddeb1d57354685
asmaaHamdym <asmahamdym@gmail.com>
2024-12-06 13:56:33 +0200

    Basic Setup

--- src/client/index.js
+++ src/client/index.js
@@ -2,12 +2,11 @@
 import { handleSubmit } from "./js/formHandler";
 import { checkForName } from "./js/nameChecker";
 
+// sass files
 import "./styles/resets.scss";
 import "./styles/base.scss";
 import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
-// alert("I EXIST")
-// console.log("CHANGE!!");
 
-// sass files
+console.log("CHANGE!!");
